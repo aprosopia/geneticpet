a35712c2e6770415259ab41f9f07fd27ae2b75c2
aprosopia <aprosopia@gmail.com>
2017-12-18 17:05:43 +0200

    DB Support
    - hsqldb and hikari db pool
    - flywaydb migrations
    - initial db schema

---
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -0,0 +1,24 @@
+CREATE TABLE DISEASE (
+  id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name        VARCHAR(256),
+  system      VARCHAR(256),
+  description VARCHAR(256)
+
+);
+
+
+CREATE TABLE BREED (
+  id   INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(256)
+);
+
+
+CREATE TABLE DISEASES_AFFECTING_BREEDS (
+  disease_id INTEGER,
+  breed_id   INTEGER,
+  FOREIGN KEY (disease_id) REFERENCES DISEASE (id),
+  FOREIGN KEY (breed_id) REFERENCES BREED (id)
+);
+
+
+
