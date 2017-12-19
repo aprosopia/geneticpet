7da3b893d6519953d1f03c0ab8993d4db5f6b160
aprosopia <aprosopia@gmail.com>
2017-12-19 19:08:25 +0200

    add more utility methods and some javadoc

--- src/main/java/com/geneticpet/site/database/DbUtil.java
+++ src/main/java/com/geneticpet/site/database/DbUtil.java
@@ -7,6 +7,15 @@ import java.util.List;
 
 public class DbUtil {
 
+    /**
+     *
+     * Converts a ResultSet into List of A
+     *
+     * @param rs
+     * @param converter
+     * @param <A>
+     * @return
+     */
     public static <A> List<A> asList(ResultSet rs, RowConverter<A> converter) {
 
         ArrayList<A> list = new ArrayList<>();
@@ -22,5 +31,39 @@ public class DbUtil {
 
         return list;
 
+    }
+
+
+    /**
+     * Get a single A. If there are no results, return null.
+     */
+    public static <A> A getSingle(ResultSet rs, RowConverter<A> converter) {
+
+        try {
+            if (!rs.next()) {
+                return null;
+            }
+            return converter.convert(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * Get a unique A. If there are no results, return null. If there is more than one result, throw IllegalStateException
+     */
+    public static <A> A getUnique(ResultSet rs, RowConverter<A> converter) {
+
+
+        A result = getSingle(rs, converter);
+        try {
+            if (rs.next()) {
+                throw new IllegalStateException("more than 1 result");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 }
